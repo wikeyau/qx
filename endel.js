8b4f3af3625cb8176eaa9cace82a884633966f44
wikeyau <121499378+wikeyau@users.noreply.github.com>
2023-12-21 18:41:58 +0000

    Create endel.js

---
+++ endel.js
@@ -0,0 +1,18 @@
+var obj = JSON.parse($response.body);
+
+obj["subscription"]= {
+        "promo_type": "DEFAULT",
+        "period": "LIFETIME",
+        "store_trial": false,
+        "time_left": 9685854580,
+        "price_id": "unknown",
+        "valid_until": 9669710512,
+        "type": "ACTIVE",
+        "trial_type": "CALENDAR_BASED",
+        "cancel_at_period_end": true,
+        "multiple_subscriptions": true,
+        "store": "APP_STORE",
+        "trial_canceled": true
+    };
+
+$done({body: JSON.stringify(obj)});
