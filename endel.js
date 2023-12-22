5ae27dcc79b153e02db5426b77e8d0f0553b1a0c
wikeyau <121499378+wikeyau@users.noreply.github.com>
2023-12-22 18:33:37 +0000

    Update endel.js

--- endel.js
+++ endel.js
@@ -38,12 +38,12 @@ obj.dynamic_variables.forEach(function(variable) {
     // if (variable.value_name && variable.value_name.toLowerCase().includes("referral")) {
     //     variable.value = "";
     // }    
-    if (variable.value_name && variable.value_name.toLowerCase().includes("unsubscribed")) {
-        variable.value = "";
-    }  
-    // if (variable.name && variable.name.toLowerCase().includes("tutorial")) {
+    // if (variable.value_name && variable.value_name.toLowerCase().includes("unsubscribed")) {
     //     variable.value = "";
-    // }
+    // }  
+    if (variable.name && variable.name.toLowerCase().includes("tutorial")) {
+        variable.value = "";
+    }
 });
 
 obj["subscription"]= {
