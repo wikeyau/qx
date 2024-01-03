7382bc314fa18980644c13b3afa5ff21ee2ef846
Mike Y <wikeyau@gmail.com>
2024-01-03 18:15:24 +0000

    Update shanqiuyuedu.js

--- shanqiuyuedu.js
+++ shanqiuyuedu.js
@@ -1,7 +1,7 @@
 /*
-QX: ^https:\/\/api\.revenuecat\.com\/v1\/subscribers\/ url script-response-body revenuecat.js
+QX: ^https:\/\/m\.815616\.xyz\/api\/v2\/myinfo\/ url script-response-body shangqiuyuedu.js
 
-hostname = api.revenuecat.com
+hostname = *.815616.com
 */
 
 let obj = JSON.parse($response.body);
@@ -11,7 +11,7 @@ if (url.includes("21df7c478")) {
     obj.data.forEach(element => {
         if (element.uuid === "21df7c478") {
             element.banned = "0";
-            element.vipto: "2023-10-05 07:32:26";
+            element.vipto: "2099-10-05 07:32:26";
         }
     });
 }
