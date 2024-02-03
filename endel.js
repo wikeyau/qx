f04b31883514678622df884fedad8a66da4b6a77
wikeyau <121499378+wikeyau@users.noreply.github.com>
2024-02-02 21:23:43 -0300

    Update endel.js

--- endel.js
+++ endel.js
@@ -41,18 +41,19 @@ if (obj.hasOwnProperty("dynamic_variables")) {
     });
 
     obj.subscription = {
-        promo_type: "DEFAULT",
-        period: "LIFETIME",
-        store_trial: false,
-        time_left: 9685854580,
-        price_id: "unknown",
-        valid_until: 9669710512,
-        type: "ACTIVE",
-        trial_type: "CALENDAR_BASED",
-        cancel_at_period_end: true,
-        multiple_subscriptions: true,
-        store: "APP_STORE",
-        trial_canceled: true
+        "promo_type" : "DEFAULT",
+        "period" : "YEAR",
+        "store_trial" : false,
+        "time_left" : 0,
+        "price_id" : "12_Months_Instant_Offer",
+        "valid_until" : 4092599349,
+        "type" : "ACTIVE",
+        "trial_type" : "CALENDAR_BASED",
+        "prev_store" : "NOSTORE",
+        "cancel_at_period_end" : false,
+        "multiple_subscriptions" : false,
+        "store" : "APP_STORE",
+        "trial_canceled" : false
     };
 }
 
